6edda6b18f8bfc60aaa548e5df67af5d7db1588e
Tomas Jansson <tomas.jansson@gmail.com>
2025-05-04 00:38:42 +0200

    Some debug logging

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -2,6 +2,10 @@ import { NextConfig } from "next";
 
 const MIDDLEWARE_FILTER_DOMAN = process.env.NEXT_DEMO_MIDDLEWARE_FILTER_DOMAIN!;
 
+console.log(
+  "[next.config.ts] MIDDLEWARE_FILTER_DOMAN",
+  MIDDLEWARE_FILTER_DOMAN
+);
 const nextConfig: NextConfig = {
   rewrites: async () => [
     {
