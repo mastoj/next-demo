400cf06f959e2d3b2024b89425e853a2829379be
Tomas Jansson <tomas.jansson@gmail.com>
2025-05-04 00:33:46 +0200

    feat(next.config): update middleware filter destinations to use environment variable

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,14 +1,16 @@
 import { NextConfig } from "next";
 
+const MIDDLEWARE_FILTER_DOMAN = process.env.NEXT_DEMO_MIDDLEWARE_FILTER_DOMAIN!;
+
 const nextConfig: NextConfig = {
   rewrites: async () => [
     {
       source: "/middleware-filter/:path*",
-      destination: "http://localhost:3001/middleware-filter/:path*",
+      destination: `${MIDDLEWARE_FILTER_DOMAN}/middleware-filter/:path*`,
     },
     {
       source: "/middleware-filter-static/:path*",
-      destination: "http://localhost:3001/middleware-filter-static/:path*",
+      destination: `${MIDDLEWARE_FILTER_DOMAN}/middleware-filter-static/:path*`,
     },
   ],
 };
