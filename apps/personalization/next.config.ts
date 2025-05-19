e410b361dafdd6651de0f0e494ed22f477fe9f5b
Tomas Jansson <tomas.jansson@gmail.com>
2025-05-19 09:14:25 +0200

    Cleanup

--- apps/personalization/next.config.ts
+++ apps/personalization/next.config.ts
@@ -4,12 +4,6 @@ const nextConfig: NextConfig = {
   transpilePackages: ["@repo/ui"],
   /* config options here */
   assetPrefix: "/x-static-personalization",
-  // rewrites: async () => [
-  //   {
-  //     source: "/x-static-middleware-filter/:path*",
-  //     destination: "/_next/static/:path*",
-  //   },
-  // ],
 };
 
 export default nextConfig;
